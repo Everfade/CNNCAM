19d712afe37a655f39edfa9b99742e0a5462fe3f
Everfade <danwkg200@hotmail.com>
2023-07-14 10:45:56 +0200

    changed commet

--- main.py
+++ main.py
@@ -23,8 +23,41 @@ class CustomCallback(tf.keras.callbacks.Callback):
  
 if __name__ == '__main__':
     tf.config.threading.set_inter_op_parallelism_threads(1)
+   
+    data_size, wspan, hspan = (2100, 10, 10)
+    x_values = np.random.choice([0, 1], (data_size, wspan, hspan), p=[.5, .5])
+    array = np.random.choice([0, 1], size=(10, 10))
+
+    gol = CaMemory(grid_size=10, initial_state=array, rule_type=RuleTypes.OuterTotalistic,
+                   neighbourhood_type=CaNeighbourhoods.Von_Neumann
+                   , memory_type=MemoryTypes.Default)
+    gol_m = CaMemory(grid_size=10, initial_state=array, rule_type=RuleTypes.OuterTotalistic,
+                     neighbourhood_type=CaNeighbourhoods.Von_Neumann
+                     , memory_type=MemoryTypes.Most_Frequent, memory_horizon=3)
+
+    gol.set_rule([[0, 0, 0, 1, 0, 0, 0, 0, 0], [0, 0, 1, 1, 0, 0, 0, 0, 0]])
+    gol_m.set_rule([[0, 0, 0, 1, 0, 0, 0, 0, 0], [0, 0, 1, 1, 0, 0, 0, 0, 0]])
+    
+   
  
-    SEED =1
+    
+   # y_values = gol.generate_training_data(x_values)
+   # gol implementation is confirmed to be equivalent
+    gol_gp = make_game_of_life()
+    y_values = gol_gp(tf.convert_to_tensor(x_values, tf.float32))
+
+    x_values_tf = tf.convert_to_tensor(x_values, tf.float32)
+    y_values_tf = tf.convert_to_tensor(y_values, tf.float32)
+    X_data = x_values_tf[..., tf.newaxis]
+    Y_data = y_values_tf[..., tf.newaxis]
+    layer_dims = [10, 10, 10]
+    #DO NOT CHANGE 
+    num_classes = 2
+
+    Y_onehot = tf.squeeze(tf.one_hot(tf.cast(Y_data, tf.int32), num_classes))
+    X_train, X_test, Y_train, Y_test = train_test_split(np.array(X_data ),np.array(Y_onehot), test_size=0.75, random_state=42)
+    X_val, X_test, Y_val, Y_test = train_test_split(np.array(X_train ),np.array(Y_train), test_size=0.20, random_state=42)
+    SEED = 7
 
     os.environ['PYTHONHASHSEED']=str(SEED)
     os.environ['TF_CUDNN_DETERMINISTIC'] = '1'  # TF 2.1
@@ -32,7 +65,8 @@ if __name__ == '__main__':
     np.random.seed(SEED)
     tf.random.set_seed(SEED)
     array = np.random.choice([0, 1], size=(10, 10))
-    gol_test_state=[[0., 1., 0., 1., 1., 0., 1., 0., 1., 0.],
+    gol_test_state=[
+       [0., 1., 0., 1., 1., 0., 1., 0., 1., 0.],
        [1., 1., 1., 0., 0., 1., 0., 0., 1., 0.],
        [1., 0., 1., 0., 0., 1., 1., 1., 1., 0.],
        [1., 1., 1., 1., 0., 1., 0., 1., 0., 0.],
@@ -43,66 +77,30 @@ if __name__ == '__main__':
        [0., 1., 1., 1., 1., 1., 0., 1., 0., 1.],
        [0., 1., 1., 0., 0., 0., 0., 1., 1., 1.]]
     gol_test_state=np.array(gol_test_state,dtype=int)
- 
- 
-  
-    array = np.random.choice([0, 1], size=(10, 10))
-    gol = CaMemory(grid_size=10, initial_state=array, rule_type=RuleTypes.OuterTotalistic,
-                   neighbourhood_type=CaNeighbourhoods.Von_Neumann
-                   , memory_type=MemoryTypes.Default)
-    gol_m = CaMemory(grid_size=10, initial_state=array, rule_type=RuleTypes.OuterTotalistic,
-                     neighbourhood_type=CaNeighbourhoods.Von_Neumann
-                     , memory_type=MemoryTypes.Most_Frequent, memory_horizon=3)
-   
-   #game of life rule
-    gol.set_rule([[0, 0, 0, 1, 0, 0, 0, 0, 0], [0, 0, 1, 1, 0, 0, 0, 0, 0]])
-    gol_m.set_rule([[0, 0, 0, 1, 0, 0, 0, 0, 0], [0, 0, 1, 1, 0, 0, 0, 0, 0]])
-    
-    data_size, wspan, hspan = (200, 10, 10)
-    x_values = np.random.choice([0, 1], (data_size, wspan, hspan), p=[.5, .5])
- 
- 
-    
-    y_values = gol.generate_training_data(x_values)
-    #gol implementation is confirmed to be equivalent
-    # gol_gp = make_game_of_life()
-    # y_values = gol_gp(tf.convert_to_tensor(x_values, tf.float32))
-
-    x_values_tf = tf.convert_to_tensor(x_values, tf.float32)
-    y_values_tf = tf.convert_to_tensor(y_values, tf.float32)
-    X_data = x_values_tf[..., tf.newaxis]
-    Y_data = y_values_tf[..., tf.newaxis]
-    layer_dims = [10, 10, 10]
-    num_classes = 2
-
-    Y_onehot = tf.squeeze(tf.one_hot(tf.cast(Y_data, tf.int32), num_classes))
-    X_train, X_test, Y_train, Y_test = train_test_split(np.array(X_data ),np.array(Y_onehot), test_size=0.20, random_state=42)
-    X_val, X_test, Y_val, Y_test = train_test_split(np.array(X_train ),np.array(Y_train), test_size=0.50, random_state=42)
-
-   
 
     loss = lambda x, y: tf.keras.losses.categorical_crossentropy(tf.reshape(x, shape=(-1, num_classes)),
                                                                  tf.reshape(y, shape=(-1, num_classes)),
                                                                  from_logits=True)
+    
+    loss_ms_error = lambda x, y: tf.keras.losses.MeanSquaredError()(tf.reshape(x, shape=(-1, num_classes)),
+                                                               tf.reshape(y, shape=(-1, num_classes)))
 
-    model = initialize_model((wspan, hspan), layer_dims, num_classes=num_classes)
+
+    model = initialize_model((wspan, hspan), layer_dims, num_classes=num_classes,totalistic=True)
  
-    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3), loss=loss)
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-4),loss=loss_ms_error, metrics=[accuracy])
 
+
+    early_stopping_callback = CustomCallback()
     #model.summary()
     #### Run training
-  
+
     Y_val_onehot = tf.squeeze(tf.one_hot(tf.cast(Y_val, tf.int32), num_classes))
-    print(X_val.shape)
-    print(X_test.shape)
-    print(Y_val.shape)
-    print(Y_test.shape)
-    print(Y_val_onehot.shape)
-    #TODO  add early stopping
+  
     callback = tf.keras.callbacks.EarlyStopping(monitor='accuarcy', patience=3)
-    train_history = model.fit(x=X_train, y=Y_train,validation_data=(X_val,Y_val) ,
-                               epochs=100, batch_size=10, verbose=1)
-    print(train_history.history["loss"][:-3])
+    train_history = model.fit(x=X_train, y=Y_train ,validation_data=(X_val,Y_val ) ,
+                               epochs=1500, batch_size=10, verbose=1 ,callbacks=[early_stopping_callback])
+    #print(train_history.history["loss"])
     #plt.plot(train_history.history['loss'], 'k')
     #plt.show()
 
@@ -110,7 +108,7 @@ if __name__ == '__main__':
  
     #activation is relu, values not between  0 and 1?
     predictions = model.predict(X_test)
-    
+  
     final=[]
     errors=[]
     for grid in predictions:
@@ -118,7 +116,7 @@ if __name__ == '__main__':
          res=[-1,-1]
          res[np.argmax(row)]=1
          res[np.argmin(row)]=0
-         #TODO Somes Rows give 0 for both cell state probabilities if the nn didnt reach accs of 100 Oddly enough replacing them this way achieves 100% often
+         #Only relevant if netowrk does not acchieve 100% accuracy
          if(res[0]==-1 or res[1]==-1):
              res[0]=1
              res[1]=0
